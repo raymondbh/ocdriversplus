6556794a4b5ec2d68694e52acdbf144f967e2fbe
Raymond B. Hansen <raymondbh@gmail.com>
2017-04-21 22:23:04 +0200

    Cleaned up a little. Added build.properties.

--- src/main/java/org/rbh/ocic2drivers/OCIC2Drivers.java
+++ src/main/java/org/rbh/ocic2drivers/OCIC2Drivers.java
@@ -6,11 +6,13 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 
-@Mod(modid = OCIC2Drivers.MODID, version = OCIC2Drivers.VERSION, serverSideOnly = true, acceptableRemoteVersions = "*", dependencies = "after:OpenComputers")
+
+
+@Mod(modid = OCIC2Drivers.MODID, version = OCIC2Drivers.VERSION, useMetadata = true, acceptableRemoteVersions = "*", serverSideOnly = true)
 public class OCIC2Drivers
 {
-    public static final String MODID = "ocic2drivers";
-    public static final String VERSION = "1.0";
+    public static final String MODID = "@MOD_ID@";
+    public static final String VERSION = "@MOD_VERSION@";
 
     @EventHandler
     public void init(FMLInitializationEvent event)
